9506c4937e38dc31256de22818f3af0a0ce6d356
Ben Wagner <benjaminwagner@google.com>
2021-06-17 20:06:49 +0000

    Workingish WebGPU sample

--- src/content/insertable-streams/video-processing/js/webgpu-transform.js
+++ src/content/insertable-streams/video-processing/js/webgpu-transform.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const wgslShaders = {
-    vertex: `
+  vertex: `
 struct VertexInput {
   [[location(0)]] position : vec3<f32>;
   [[location(1)]] uv : vec2<f32>;
@@ -17,234 +17,248 @@ struct VertexOutput {
 fn main(input : VertexInput) -> VertexOutput {
     var output : VertexOutput;
     output.Position = vec4<f32>(input.position, 1.0);
-    output.fragUV = vec2<f32>(-0.5,-0.0) + input.uv;
+    output.fragUV = input.uv;
     return output;
 }
 `,
 
-fragment: `
+  fragment: `
 [[binding(0), group(0)]] var mySampler: sampler;
 [[binding(1), group(0)]] var myTexture: texture_2d<f32>;
 
 [[stage(fragment)]]
 fn main([[location(0)]] fragUV : vec2<f32>) -> [[location(0)]] vec4<f32> {
-  return textureSample(myTexture, mySampler, fragUV);
+  let angle = pow(max(0.5 - distance(fragUV, vec2<f32>(0.5)), 0.0) * 2.0, 2.0);
+  let rotation = vec2<f32>(sin(angle), cos(angle));
+  let fromCenter = fragUV - vec2<f32>(0.5);
+  let rotatedPosition = vec2<f32>(
+    fromCenter.x * rotation.y + fromCenter.y * rotation.x,
+    fromCenter.y * rotation.y - fromCenter.x * rotation.x) + vec2<f32>(0.5);
+  let quadrants = vec2<f32>(rotatedPosition.x * 2.0,
+                            rotatedPosition.y * 2.0);
+  return textureSample(myTexture, mySampler, quadrants);
 }
 `,
 };
 
+function webGPUNotSupported() {
+  alert(
+      'Your browser does not support WebGPU. See the note at the bottom of the page.');
+}
+
+const swapChainFormat = 'bgra8unorm';
+
 class WebGPUTransform {
-    constructor() {
-        this.canvas_ = null;
-        this.device_ = null;
-        this.renderPipeline_ = null;
-        this.swapChain_ = null;
-        /** GPU buffer */
-        this.verticesBuffer_ = null;
-        /**
-         * @private {boolean} If false, pass VideoFrame directly to
-         * WebGLRenderingContext.texImage2D and create VideoFrame directly from
-         * this.canvas_. If either of these operations fail (it's not supported in
-         * Chrome <90 and broken in Chrome 90: https://crbug.com/1184128), we set
-         * this field to true; in that case we create an ImageBitmap from the
-         * VideoFrame and pass the ImageBitmap to texImage2D on the input side and
-         * create the VideoFrame using an ImageBitmap of the canvas on the output
-         * side.
-         */
-        this.use_image_bitmap_ = true; // Earlier value is false
-        /** @private {string} */
-        this.debugPath_ = 'debug.pipeline.frameTransform_';
+  constructor() {
+    this.canvas_ = null;
+    this.device_ = null;
+    this.renderPipeline_ = null;
+    /** GPU buffer */
+    this.verticesBuffer_ = null;
+    /** @private {string} */
+    this.debugPath_ = 'debug.pipeline.frameTransform_';
+  }
+
+  /** @override */
+  async init() {
+    console.log('[WebGPUTransform] Initializing WebGPU.');
+
+    this.canvas_ = new OffscreenCanvas(1, 1);
+    // Uncomment these lines to get a visible canvas.
+    //this.canvas_ = document.createElement('canvas');
+    //document.getElementById('outputVideoContainer').appendChild(this.canvas_);
+
+    this.context_ = this.canvas_.getContext('gpupresent');
+    if (!this.context_) {
+      webGPUNotSupported()
+      return;
+    }
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      webGPUNotSupported()
+      return;
+    }
+    this.device_ = await adapter.requestDevice();
+    if (!this.device_) {
+      webGPUNotSupported();
+      return;
     }
 
-    async init() {
-        
-        console.log('[WebGPUTransform] Initializing WebGPU.');
-        
-        this.canvas_ = document.createElement('canvas');
-        this.canvas_.width = 5000;
-        this.canvas_.height = 2500;
+    // Copies rectVerts to this.verticesBuffer_
+    const rectVerts = new Float32Array([
+      // position
+      1.0, 1.0, 0.0,    //
+      1.0, 0.0, 1.0,    //
+      -1.0, 0.0, 1.0,   //
+      1.0, -1.0, -1.0,  //
+      0.0, 0.0, 1.0,    //
+      1.0, 1.0, 0.0,    //
+      // uv
+      1.0, 0.0,    //
+      -1.0, -1.0,  //
+      0.0, 0.0,    //
+      1.0, -1.0,   //
+      1.0, 0.0,    //
+      0.0, 0.0,    //
+    ]);
+    this.verticesBuffer_ = this.device_.createBuffer({
+      size: rectVerts.byteLength,
+      usage: GPUBufferUsage.VERTEX,
+      mappedAtCreation: true,
+    });
+    new Float32Array(this.verticesBuffer_.getMappedRange()).set(rectVerts);
+    this.verticesBuffer_.unmap();
 
-        const canvas = this.canvas_;
-        const context = canvas.getContext('gpupresent');
-        const adapter = await navigator.gpu.requestAdapter();
-        const device = await adapter.requestDevice();
-        this.device_ = device;
-        if(this.device_ == null) return;
-
-        // Copies rectVerts to this.verticesBuffer_
-        const rectVerts = new Float32Array([
-            1.0, 1.0, 0.0, 1.0, 0.0,
-            1.0, -1.0, 0.0, 1.0, 1.0,
-            -1.0, -1.0, 0.0, 0.0, 1.0,
-            1.0, 1.0, 0.0, 1.0, 0.0,
-            -1.0, -1.0, 0.0, 0.0, 1.0,
-            -1.0, 1.0, 0.0, 0.0, 0.0,
-        ]);
-        this.verticesBuffer_ = device.createBuffer({
-            size: rectVerts.byteLength,
-            usage: GPUBufferUsage.VERTEX,
-            mappedAtCreation: true,
-        });
-        new Float32Array(this.verticesBuffer_.getMappedRange()).set(rectVerts);
-        this.verticesBuffer_.unmap();
-
-        const swapChainFormat = 'bgra8unorm';
-        this.swapChain_ = context.configureSwapChain({
-            device,
+    this.renderPipeline_ = this.device_.createRenderPipeline({
+      vertex: {
+        module: this.device_.createShaderModule({
+          code: wgslShaders.vertex,
+        }),
+        entryPoint: 'main',
+        buffers: [
+          {
+            arrayStride: 20,
+            attributes: [
+              {
+                // position
+                shaderLocation: 0,
+                offset: 0,
+                format: 'float32x3',
+              },
+              {
+                // uv
+                shaderLocation: 1,
+                offset: 12,
+                format: 'float32x2',
+              },
+            ],
+          },
+        ],
+      },
+      fragment: {
+        module: this.device_.createShaderModule({
+          code: wgslShaders.fragment,
+        }),
+        entryPoint: 'main',
+        targets: [
+          {
             format: swapChainFormat,
-        });
-        
-        this.renderPipeline_ = device.createRenderPipeline({
-            vertex: {
-                module: device.createShaderModule({
-                    code: wgslShaders.vertex,
-                }),
-                entryPoint: 'main',
-                buffers: [
-                    {
-                        arrayStride: 20,
-                        attributes: [
-                            {
-                                // position
-                                shaderLocation: 0,
-                                offset: 0,
-                                format: 'float32x3',
-                            },
-                            {
-                                // uv
-                                shaderLocation: 1,
-                                offset: 12,
-                                format: 'float32x2',
-                            },
-                        ],
-                    },
-                ],
-            },
-            fragment: {
-                module: device.createShaderModule({
-                    code: wgslShaders.fragment,
-                }),
-                entryPoint: 'main',
-                targets: [
-                    {
-                        format: swapChainFormat,
-                    },
-                ],
-            },
-            primitive: {
-                topology: 'triangle-list',
-            },
-        });        
+          },
+        ],
+      },
+      primitive: {
+        topology: 'triangle-list',
+      },
+    });
 
+    this.sampler_ = this.device_.createSampler({
+      addressModeU: 'repeat',
+      addressModeV: 'repeat',
+      addressModeW: 'repeat',
+      magFilter: 'linear',
+      minFilter: 'linear',
+    });
+  }
+
+  setSize(width, height) {
+    this.canvas_.width = width;
+    this.canvas_.height = height;
+    this.context_.configure({
+      device: this.device_,
+      format: swapChainFormat,
+    });
+  }
+
+  /** @override */
+  async transform(frame, controller) {
+    if (!this.device_ || !this.canvas_) {
+      frame.close();
+      return;
+    }
+    const width = frame.displayWidth;
+    const height = frame.displayHeight;
+    if (this.canvas_.width !== width || this.canvas_.height !== height) {
+      this.setSize(width, height);
     }
 
-    async transform(frame, controller) {
-        const device = this.device_;
-        if(device == null) init();
-        const canvas = this.canvas_;
-        const sampler = device.createSampler({
-            addressModeU: 'repeat',
-            addressModeV: 'repeat',
-            addressModeW: 'repeat',
-            magFilter: 'linear',
-            minFilter: 'linear',
+    // Something like this should work, but isn't implemented yet:
+    // const videoTexture = this.device_.experimentalImportTexture(frame,
+    // GPUTextureUsage.SAMPLED);
+
+    // Instead we use ImageBitmap as an intermediary.
+    const size = {
+      width,
+      height,
+      depthOrArrayLayers: 1,
+    };
+    const videoTexture = this.device_.createTexture({
+      size,
+      arrayLayerCount: 1,
+      mipLevelCount: 1,
+      sampleCount: 1,
+      dimension: '2d',
+      format: 'rgba8unorm',
+      usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.SAMPLED,
+    });
+    const imageBitmap = await createImageBitmap(frame);
+    this.device_.queue.copyImageBitmapToTexture(
+        {imageBitmap, origin: {x: 0, y: 0}},
+        {texture: videoTexture, origin: {x: 0, y: 0}}, {
+          width,
+          height,
         });
+    imageBitmap.close();
 
-        const videoFrame = await createImageBitmap(frame);
-        const videoTexture = device.createTexture({size: {
-            width: frame.displayWidth*2,
-            height: frame.displayHeight*2,
-            depthOrArrayLayers: 1,
-        }, arrayLayerCount: 1,
-        mipLevelCount: 1,
-        sampleCount: 1,
-        dimension: '2d',
-        format: 'rgba8unorm',
-        usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.SAMPLED,});
-        
-        device.queue.copyImageBitmapToTexture(
-            { 
-                imageBitmap: videoFrame, 
-                origin: { x: 0, y: 0 } 
-            },
-            { 
-                texture: videoTexture, 
-                origin: { x: 0, y: 0 }  
-            },
-            {
-                width: frame.displayWidth,
-                height: frame.displayHeight,
-            }
-        );
-        
-        device.queue.copyImageBitmapToTexture(
-            { 
-                imageBitmap: videoFrame, 
-                origin: { x: 0, y: 0 } 
-            },
-            { 
-                texture: videoTexture ,
-                origin: 
-                { 
-                    x: frame.displayWidth, 
-                    y: frame.displayHeight 
-                }  
-            },
-            {
-                width: frame.displayWidth,
-                height: frame.displayHeight,
-            }
-        );
 
-        const uniformBindGroup = device.createBindGroup({
-            layout: this.renderPipeline_.getBindGroupLayout(0),
-            entries: [
-                {
-                    binding: 0,
-                    resource: sampler,
-                },
-                {
-                    binding: 1,
-                    resource: videoTexture.createView(),
-                },
-            ],
-        });
 
-        const commandEncoder = device.createCommandEncoder();
-        const textureView = this.swapChain_.getCurrentTexture().createView();
+    const uniformBindGroup = this.device_.createBindGroup({
+      layout: this.renderPipeline_.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: this.sampler_,
+        },
+        {
+          binding: 1,
+          resource: videoTexture.createView(),
+        },
+      ],
+    });
 
-        const renderPassDescriptor = {
-            colorAttachments: [
-                {
-                    view: textureView,
-                    loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-                    storeOp: 'store',
-                },
-            ],
-        };
-        const timestamp = frame.timestamp;
-        frame.close();
-        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-        passEncoder.setPipeline(this.renderPipeline_);
-        passEncoder.setVertexBuffer(0, this.verticesBuffer_);
-        passEncoder.setBindGroup(0, uniformBindGroup);
-        passEncoder.draw(6, 1, 0, 0);
-        passEncoder.endPass();
-        device.queue.submit([commandEncoder.finish()]);
+    const timestamp = frame.timestamp;
+    frame.close();
 
-        document.body.appendChild(canvas);
-        // controller.enqueue(new VideoFrame(canvas, {timestamp}));
+    const commandEncoder = this.device_.createCommandEncoder();
 
-        // const outputBitmap = await createImageBitmap(canvas);
-        const outputBitmap = await createImageBitmap(videoFrame);
-        const outputFrame = new VideoFrame(outputBitmap, {timestamp});
-        outputBitmap.close();
-        controller.enqueue(outputFrame);
-    }
+    const textureView = this.context_.getCurrentTexture().createView();
+    const renderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          loadValue: {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+          storeOp: 'store',
+        },
+      ],
+    };
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setViewport(0, 0, width, height, 1, 1);
+    passEncoder.setPipeline(this.renderPipeline_);
+    passEncoder.setVertexBuffer(0, this.verticesBuffer_);
+    passEncoder.setBindGroup(0, uniformBindGroup);
+    passEncoder.draw(6);
+    passEncoder.endPass();
+    this.device_.queue.submit([commandEncoder.finish()]);
 
-    /** @override */
-    async destroy() {
-        console.log('[WebGPUTransform] Forcing WebGPU context to be lost.');
-        const destroy = await this.device_.destroy();
-    }
+    // This should work, but it doesn't.
+    controller.enqueue(new VideoFrame(this.canvas_, {timestamp}));
+    videoTexture.destroy();
+  }
+
+  /** @override */
+  async destroy() {
+    console.log('[WebGPUTransform] Freeing all WebGPU resources.');
+    this.verticesBuffer_.destroy();
+    // this.context_.unconfigure();
+    this.device_ = null;
+  }
 }
